a99650a9ff05cda0cf1efd85e177533d68c6e98b
Domen Šoberl <domen.soberl@gmail.com>
2022-11-27 15:15:13 +0100

    Added cart-pole simulator and the pendulum DDPG example.

---
+++ ddpg/example/pendulum.py
@@ -0,0 +1,82 @@
+import gym
+import numpy as np
+from ddpg import DDPG
+import tensorflow as tf
+import random, math, time
+
+def process_state(state):
+    (x, y, thetadot) = state
+    theta = math.atan2(y, x)
+    return (theta, thetadot)
+
+def new_ddpg():
+    return DDPG(
+        num_inputs=2,
+        num_outputs=1,
+        noise=[0.01],
+        actor_layers=[64, 32],
+        critic_layers=[64, 32],
+        memory_size=100000
+    )
+
+env = gym.make('Pendulum-v1')
+ddpg = new_ddpg()
+
+if ddpg.load_weights("pendulum-model"):
+    print("Weights loaded.")
+
+episode_count = 0
+episode_steps = 0
+episode_reward = 0
+
+state = process_state(env.reset())
+
+episodes = 100
+repetitions = 1
+n = 0
+
+rewards = np.zeros(episodes)
+
+while True:
+    env.render()
+
+    action = ddpg.action(state)
+
+    state, reward, done, info = env.step(2.0*action)
+    state = process_state(state)
+
+    episode_steps += 1
+    episode_reward += reward
+
+    ddpg.feed(action, reward, state)
+    ddpg.train()
+
+    if done:
+        print("Episode {} finished in {} steps, average reward = {}".format(
+            episode_count, episode_steps, episode_reward / episode_steps))
+        
+        rewards[episode_count] += episode_reward / episode_steps
+        
+        episode_count += 1
+        episode_steps = 0
+        episode_reward = 0
+        locked_direction = 0
+        
+        state = process_state(env.reset())
+        ddpg.update_target_networks()
+
+        if episode_count >= episodes:
+            n += 1
+            if n >= repetitions:
+                break
+
+            print("Repetition", n)
+            ddpg = new_ddpg()
+            episode_count = 0
+
+    time.sleep(0.02)
+
+for i in range(episodes):
+    print(rewards[i] / n)
+
+ddpg.save_weights("pendulum-model")
---
+++ simulator/python/cart.py
@@ -0,0 +1,109 @@
+import math
+
+class Cart:
+    width = 1.0
+    cart_weight = 10.0
+    pole_weight = 1.0
+    pole_length = 1.0
+    position = (0.0, 0.0)
+    speed = 0.0
+    acceleration = 0.0
+    damping = 0
+    theta = 0.0
+    theta_speed = 0.0
+    theta_acceleration = 0.0
+    theta_damping = 0
+    actions_per_second = 20
+    _time_since_action = 0.0
+    _last_action = 0.0
+
+    def __init__(self):
+        pass
+
+    def stop(self):
+        self.speed = 0.0
+        self.acceleration = 0.0
+
+    def straighten(self):
+        self.theta = 0.0
+        self.theta_speed = 0.0
+        self.theta_acceleration = 0.0
+
+    def move_by(self, position):
+        (x, y) = position
+        x0, y0 = self.position
+        self.position = (x0 + x, y0 + y)
+
+    def reset(self):
+        self.stop()
+        self.straighten()
+        self.position = (0, 0)
+
+    def get_bumpers(self):
+        (cx, cy) = self.position
+        wheel = self.width / 10
+        x0 = cx - self.width / 2
+        y0 = cy + wheel
+        x1 = x0 + self.width
+        y1 = self.width / 6 + wheel
+        y = (y0 + y1) / 2.0
+        return [(x0, y), (x1, y)]
+
+    def collides_with(self, position):
+        (x, y) = position
+        (cx, cy) = self.position
+        wheel = self.width / 10
+        x0 = cx - self.width / 2
+        y0 = cy + wheel
+        x1 = x0 + self.width
+        y1 = self.width / 6 + wheel
+        return x0 <= x <= x1 and y0 <= y <= y1
+
+    def tick(self, f, g, dt):
+        self._time_since_action += dt
+        (x, y) = self.position
+
+        rad = math.radians(self.theta)
+        sin = math.sin(rad)
+        cos = math.cos(rad)
+        theta_speed_rad = math.radians(self.theta_speed)
+
+        a = self.pole_weight * self.pole_length * math.pow(theta_speed_rad, 2) * sin
+        b = self.pole_weight * g * sin * cos
+        c = self.cart_weight + self.pole_weight - self.pole_weight * cos * cos
+        self.acceleration = (f - a + b) / c - self.damping * self.speed
+        self.speed += self.acceleration * dt
+        x += self.speed * dt
+        self.position = (x, y)
+
+        a = self.acceleration / self.pole_length * cos
+        b = g / self.pole_length * sin
+        self.theta_acceleration = math.degrees(a + b) - self.theta_damping * self.theta_speed
+        self.theta_speed += self.theta_acceleration * dt
+        self.theta += self.theta_speed * dt
+        if self.theta < 180:
+            self.theta += 360
+        if self.theta >= 180:
+            self.theta -= 360
+            
+    def draw(self, canvas):
+        (x, y) = self.position
+        wheel = self.width / 10
+        pole = self.width / 20
+        x0 = x - pole / 2.0
+        y0 = y + wheel + self.width / 6
+        x1 = x0 + pole
+        y1 = y0 + self.pole_length
+        canvas.draw_rectangle((x0, y0), (x1, y1), (64, 32, 32), (x, y0), self.theta)
+        canvas.draw_circle((x, y0 - 0.5 * pole), 1.2 * pole, (128, 0, 0))
+        canvas.draw_circle((x, y1), 1.5 * pole, (0, 0, 0), (x, y0), self.theta)
+
+        x0 = x - self.width / 2
+        y0 = y + wheel
+        x1 = x0 + self.width
+        y1 = y0 + self.width / 6
+        canvas.draw_rectangle((x0, y0), (x1, y1), (128, 0, 0))
+        canvas.draw_circle((x0 + 2.0 * wheel, y0), wheel, (0, 0, 0))
+        canvas.draw_circle((x0 + 2.0 * wheel, y0), wheel / 2.0, (64, 32, 32))
+        canvas.draw_circle((x1 - 2.0 * wheel, y0), wheel, (0, 0, 0))
+        canvas.draw_circle((x1 - 2.0 * wheel, y0), wheel / 2.0, (64, 32, 32))
